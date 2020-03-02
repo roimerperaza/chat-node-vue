c0069b4c316bcb6a662d8be0160fdc5854706351
Roimer Peraza <roimer@roimer.peraza>
2020-03-01 21:15:56 -0500

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="myVar" />
+    <button @click="clickButton(myVar)">Ping Server</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      myVar: ''
+    }
+  },
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
+      this.sockets.subscribe('chat message', data => {
+        console.log('data', data)
+        this.msg = data.message
+      })
+    },
+    customEmit: function (data) {
+      console.log(
+        'this method was fired by the socket server. eg: io.emit("customEmit", data)'
+      )
+    }
+  },
+  methods: {
+    clickButton: function (data) {
+      // $socket is socket.io-client instance
+      this.$socket.emit('chat message', data)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  },
+  options: {
+    path: ''
+  } // Optional options
+}))
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
